048c371d1c1640588f4aca9df08b716a690c631f
Juan Díaz <juan.diaz.12069@gmail.com>
2020-02-11 16:22:55 -0300

    add navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layouts/Navbar';
+import Forms from './components/forms/Forms';
+import FormRender from './components/render/FormRender';
+import FormBuilder from './components/builder/FormBuilder';
+import EditFormElement from './components/builder/EditFormElement';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Route exact path="/" component={Forms} />
+      <Route exact path="/forms" component={Forms} />
+      <Switch>
+        <Route exact path="/formbuilder" component={FormBuilder} />
+        <Route exact path="/formrender" component={FormRender} />
+        <Route exact path="/edit-form-element/:id" component={EditFormElement} />
+      </Switch>
+    </Router>
   );
 }
 
