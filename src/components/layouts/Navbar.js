7af90e8e604270e635fb2913134bc9ef03935970
Juan Díaz <juan.diaz.12069@gmail.com>
2020-02-12 15:37:12 -0300

    add breadcrumb

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
 
     const links = (
-        <ul>
-            <li><Link to='/forms'>Forms</Link></li>
-            <li><Link to='/formbuilder'>Form Builder</Link></li>
-            <li><Link to='/formrender'>Form Render</Link></li>
-        </ul>
+        <ol className="breadcrumb">
+            <li className="breadcrumb-item"><Link to='/forms'>Forms</Link></li>
+            <li className="breadcrumb-item"><Link to='/formbuilder'>Form Builder</Link></li>
+            <li className="breadcrumb-item"><Link to='/formrender'>Form Render</Link></li>
+        </ol>
     );
 
     return (
