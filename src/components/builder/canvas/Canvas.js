4d917bd94a7f0bccf8321f3014a635e707c3b794
Juan Díaz <juan.diaz.12069@gmail.com>
2020-02-12 15:38:25 -0300

    add jsonschema-form, add element template

--- src/components/builder/canvas/Canvas.js
+++ src/components/builder/canvas/Canvas.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react';
-import { Droppable, Draggable } from 'react-beautiful-dnd';
+import { Droppable } from 'react-beautiful-dnd';
+import CanvasElementTemplate from './CanvasElementTemplate'
+import Form from "react-jsonschema-form";
 import PropTypes from 'prop-types';
 
 // TODO:
-// Add canvas item component or something
 // Style borders on drag action
 
-const Canvas = ({ canvas }) => {
+const Canvas = ({ schema, uiSchema }) => {
     return (
         <Droppable key="Canvas" droppableId="Canvas">
             {(provided, snapshot) => (
@@ -17,53 +18,20 @@ const Canvas = ({ canvas }) => {
                             {...provided.draggableProps}
                             {...provided.dragHandleProps}
                         >
-                            {
-                                canvas.length
-                                    ? canvas.map(
-                                        (item, index) =>
-                                            (
-                                                <Draggable
-                                                    key={item.id}
-                                                    draggableId={item.id}
-                                                    index={index}>
-                                                    {(
-                                                        provided,
-                                                        snapshot
-                                                    ) => (
-                                                            <Fragment>
-                                                                <div
-                                                                    className="form-group"
-                                                                    ref={provided.innerRef}
-                                                                    {...provided.draggableProps}
-                                                                // isDragging={snapshot.isDragging}
-                                                                >
-                                                                    <div className="form-heading">
-                                                                        <label htmlFor="">Elemento</label>
-                                                                        <div className="form-tools">
-                                                                            <i className="fas fa-edit"></i>
-                                                                            <i className="fas fa-trash-alt"></i>
-                                                                            <i {...provided.dragHandleProps} className="fas fa-arrows-alt"></i>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div className="form-content">
-                                                                        <input type="text" disabled placeholder="Elemento..." />
-                                                                    </div>
-                                                                </div>
-                                                            </Fragment>
 
-                                                        )}
-                                                </Draggable>
-                                            )
-                                    )
-                                    :
-                                    // !provided.placeholder &&
-                                    (
-                                        <Fragment>
-                                            <div className="notice">
-                                                Arrastra los elementos del menu aca
-                                             </div>
-                                        </Fragment>
-                                    )}
+                            {schema.properties ?
+                                (
+                                    <Form schema={schema} uiSchema={uiSchema} FieldTemplate={CanvasElementTemplate} disabled={true}>
+                                        <div></div>
+                                    </Form>
+                                )
+                                : (
+                                    <Fragment>
+                                        <div className="notice">
+                                            Arrastra los elementos del menu aca
+                                            </div>
+                                    </Fragment>
+                                )}
                             {provided.placeholder}
                         </div>
                     </div>
@@ -74,7 +42,8 @@ const Canvas = ({ canvas }) => {
 }
 
 Canvas.propTypes = {
-    canvas: PropTypes.array.isRequired,
+    schema: PropTypes.object.isRequired,
+    uiSchema: PropTypes.object,
 }
 
 export default Canvas
