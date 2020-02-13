22b59bde98421b895d1be4571cdef8a4d7bf0652
Juan Díaz <juan.diaz.12069@gmail.com>
2020-02-13 14:19:08 -0300

    add draggable, delete and edit buttons

--- src/components/builder/canvas/CanvasElementTemplate.js
+++ src/components/builder/canvas/CanvasElementTemplate.js
@@ -1,31 +1,47 @@
 import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types';
 
 const CanvasElementTemplate = ({
     id, classNames, label, help, required, description, errors, children
 }) => {
+    const elementId = id.split('_')[1];
     return (
-        <div className={classNames}>
-            {
-                id !== 'root' ?
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <label htmlFor={id}>{label}{required ? "*" : null}</label>
-                        <div>
-                            <Link to='edit-form-element/:id'>
-                                <i className="fas fa-edit"></i>
-                            </Link>
-                            <i className="fas fa-trash-alt"></i>
-                            <i /*{...provided.dragHandleProps}*/ className="fas fa-arrows-alt"></i>
-                        </div>
-                    </div>
-                    : ''
+        <Draggable index={0} draggableId={id}>
+            {(provided, snapshot) => (
+                <div
+                    className={classNames}
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                >
+                    {
+                        id !== 'root' ?
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <label htmlFor={id}>{label}{required ? "*" : null}</label>
+                                <div>
+                                    <Link to={`edit-form-element/${elementId}`}>
+                                        <i className="fas fa-edit"></i>
+                                    </Link>
+
+                                    <Link to="#!">
+                                        <i onClick={(e) => alert(elementId)} className="fas fa-trash-alt"></i>
+                                    </Link>
+
+                                    <i {...provided.dragHandleProps} className="fas fa-arrows-alt"></i>
+                                </div>
+                            </div>
+                            : ''
+                    }
+                    {children}
+                    {id !== 'root' ? description : ''}
+                </div>
+            )
             }
-            {children}
-            {id !== 'root' ? description : ''}
-        </div >
-    );
-}
+        </Draggable >
+    )
+};
 
 CanvasElementTemplate.propTypes = {
     id: PropTypes.string.isRequired,
