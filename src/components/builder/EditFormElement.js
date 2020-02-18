00ab3e485d7f390155a48441e5e6ed62ff2e0006
Juan Díaz <juan.diaz.12069@gmail.com>
2020-02-18 17:34:00 -0300

    add proptypes, connect to store

--- src/components/builder/EditFormElement.js
+++ src/components/builder/EditFormElement.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { } from '../../actions/form';
+import { editElement } from '../../actions/form';
 import Form from "react-jsonschema-form";
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+
+//TODO:
+// do something when id not exist
 
 const EditFormElement = (props) => {
-    const id = props.match.params.id
+    const id = props.match.params.id;
+
+    const { element, required, editElement } = props;
 
     const [schema, setSchema] = useState({
         title: "Editar Elemento",
@@ -25,30 +30,46 @@ const EditFormElement = (props) => {
     const [formData, setFormData] = useState({
         title: '',
         required: false
-    })
+    });
 
     useEffect(() => {
+        if (!Object.keys(element).length) {
+            return props.history.push(`/formbuilder`)
+        }
 
-
+        const isRequired = required.find(element => element === id)
 
         setFormData({
-            title: "Campo de texto",
-            required: true
+            title: element[id].title ? element[id].title : '...titulo del elemento',
+            required: isRequired ? true : false,
         });
-    }, [])
+    }, [element, id, props, required])
+
+    const onSubmit = (e) => {
+        editElement(id, e.formData);
+        return props.history.push(`/formbuilder`);
+    }
 
     return (
         <div id="editFormElement" className="container">
             <Form
                 schema={schema}
                 formData={formData}
+                onSubmit={onSubmit}
             />
         </div>
     )
 }
 
-// EditFormElement.propTypes = {
+EditFormElement.propTypes = {
+    element: PropTypes.object.isRequired,
+    required: PropTypes.array.isRequired,
+    editElement: PropTypes.func.isRequired,
+};
 
-// }
+const mapStateToProps = state => ({
+    element: state.form.schema.properties,
+    required: state.form.schema.required,
+})
 
-export default EditFormElement
+export default connect(mapStateToProps, { editElement })(EditFormElement);
