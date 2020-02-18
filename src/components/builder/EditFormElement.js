eb617350271253451a969d252f856540c3ba939c
Juan Díaz <juan.diaz.12069@gmail.com>
2020-02-18 13:50:27 -0300

    edit form

--- src/components/builder/EditFormElement.js
+++ src/components/builder/EditFormElement.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { } from '../../actions/form';
+import Form from "react-jsonschema-form";
 // import PropTypes from 'prop-types'
 
-const EditFormElement = props => {
+const EditFormElement = (props) => {
+    const id = props.match.params.id
+
+    const [schema, setSchema] = useState({
+        title: "Editar Elemento",
+        type: "object",
+        properties: {
+            title: {
+                type: "string",
+                title: "Titulo"
+            },
+            required: {
+                type: "boolean",
+                title: "Requerido"
+            }
+        }
+    });
+
+    const [formData, setFormData] = useState({
+        title: '',
+        required: false
+    })
+
+    useEffect(() => {
+
+
+
+        setFormData({
+            title: "Campo de texto",
+            required: true
+        });
+    }, [])
+
     return (
-        <div>
-            <h2>Edit Form Element</h2>
+        <div id="editFormElement" className="container">
+            <Form
+                schema={schema}
+                formData={formData}
+            />
         </div>
     )
 }
