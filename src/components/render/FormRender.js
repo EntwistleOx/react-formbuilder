5b19d188a394ceab7b24204b802e22a0759043ce
Juan Díaz <juan.diaz.12069@gmail.com>
2020-02-19 16:22:08 -0300

    add jsonschema form to render form

--- src/components/render/FormRender.js
+++ src/components/render/FormRender.js
@@ -1,16 +1,35 @@
-import React from 'react'
-// import PropTypes from 'prop-types'
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import Form from "react-jsonschema-form";
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const Formrender = props => {
+const Formrender = ({ formSchema }) => {
+
+    const onSubmit = (e) => {
+        console.log(e)
+    }
     return (
-        <div>
-            <h2>Render</h2>
-        </div>
+        <Fragment>
+            <h2>render</h2>
+            <div className="container">
+                <Form
+                    schema={formSchema.schema}
+                    uiSchema={formSchema.uiSchema}
+                    formData={formSchema.formData}
+                >
+                </Form>
+            </div>
+        </Fragment>
     )
 }
 
-// Formrender.propTypes = {
+Formrender.propTypes = {
+    formSchema: PropTypes.object.isRequired,
+}
 
-// }
+const mapStateToProps = state => ({
+    formSchema: state.form,
+})
 
-export default Formrender
+export default connect(mapStateToProps)(Formrender)
