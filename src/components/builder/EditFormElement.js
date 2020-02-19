6d72b08ffae789711485606d067aa781b631c4e7
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-02-19 01:28:23 -0300

    add options type array, in progress

--- src/components/builder/EditFormElement.js
+++ src/components/builder/EditFormElement.js
@@ -1,92 +1,103 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { editElement } from '../../actions/form';
-import Form from "react-jsonschema-form";
+import Form from 'react-jsonschema-form';
 import PropTypes from 'prop-types';
 
 //TODO:
 // do something when id not exist
 
-const EditFormElement = (props) => {
-    const id = props.match.params.id;
+const EditFormElement = props => {
+  const id = props.match.params.id;
 
-    const { element, required, editElement } = props;
+  const { element, required, editElement } = props;
 
-    console.log(props)
+  console.log(props);
 
-    const [schema, setSchema] = useState({
-        title: "Editar Elemento",
-        type: "object",
-        properties: {
-            title: {
-                type: "string",
-                title: "Titulo"
-            },
-            required: {
-                type: "boolean",
-                title: "Requerido"
-            },
-            options: {
-                "type": "array",
-                "title": "Opciones",
-                "items": {
-                    "type": "string",
-                }
-            },
+  const [schema, setSchema] = useState({
+    title: 'Editar Elemento',
+    type: 'object',
+    properties: {
+      title: {
+        type: 'string',
+        title: 'Titulo'
+      },
+      required: {
+        type: 'boolean',
+        title: 'Requerido'
+      },
+      options: {
+        type: 'array',
+        title: 'Opciones',
+        items: {
+          type: 'array',
+          //   title: 'Inner list',
+          items: {
+            type: 'string'
+          }
         }
-    });
-
-    const [uiSchema, setuiSchema] = useState({
-
-    })
-    const [formData, setFormData] = useState({
-        title: '',
-        required: false,
-        options: []
-    });
-
-    useEffect(() => {
-        if (!Object.keys(element).length) {
-            return props.history.push(`/formbuilder`)
-        }
-
-        const isRequired = required.find(element => element === id)
-
-        setFormData({
-            title: element[id].title ? element[id].title : '...titulo del elemento',
-            required: isRequired ? true : false,
-            options: element[id].enum ? element[id].enum : [],
-        });
-    }, [element, id, props, required])
-
-    const onSubmit = (e) => {
-        editElement(id, e.formData);
-        return props.history.push(`/formbuilder`);
+      }
     }
+  });
 
-    console.log(schema)
-    console.log(formData)
-    return (
-        <div id="editFormElement" className="container">
-            <Form
-                schema={schema}
-                uiSchema={uiSchema}
-                formData={formData}
-                onSubmit={onSubmit}
-            />
-        </div>
-    )
-}
+  const [uiSchema, setuiSchema] = useState({});
+  const [formData, setFormData] = useState({
+    title: '',
+    required: false,
+    options: [['key', 'value']]
+  });
+
+  //   useEffect(() => {
+  //     if (!Object.keys(element).length) {
+  //       return props.history.push(`/formbuilder`);
+  //     }
+  //     const keys = element[id].enum;
+  //     const values = element[id].enumNames;
+  //     setFormData(Object.fromEntries(keys.map((_, i) => [keys[i], values[i]])));
+  //   }, [element, id]);
+
+  useEffect(() => {
+    if (!Object.keys(element).length) {
+      return props.history.push(`/formbuilder`);
+    }
+    const isRequired = required.find(element => element === id);
+    setFormData({
+      ...formData,
+      title: element[id].title ? element[id].title : '...titulo del elemento',
+      required: isRequired ? true : false
+    });
+  }, [element, id, props, required]);
+
+  const onSubmit = e => {
+    console.log(e.formData);
+    editElement(id, e.formData);
+    return props.history.push(`/formbuilder`);
+  };
+
+  console.log(schema);
+  console.log(formData);
+
+  return (
+    <div id='editFormElement' className='container'>
+      <Form
+        schema={schema}
+        uiSchema={uiSchema}
+        formData={formData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
+};
 
 EditFormElement.propTypes = {
-    element: PropTypes.object.isRequired,
-    required: PropTypes.array.isRequired,
-    editElement: PropTypes.func.isRequired,
+  element: PropTypes.object.isRequired,
+  required: PropTypes.array.isRequired,
+  editElement: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
-    element: state.form.schema.properties,
-    required: state.form.schema.required,
-})
+  element: state.form.schema.properties,
+  required: state.form.schema.required
+});
 
 export default connect(mapStateToProps, { editElement })(EditFormElement);
