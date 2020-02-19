3a92d5cec6e66dad677a5a6ee53324d8aa5d3a78
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-02-19 01:27:50 -0300

    typo

--- src/components/builder/toolkit/toolkitSchema.js
+++ src/components/builder/toolkit/toolkitSchema.js
@@ -2,91 +2,86 @@ import uuid from 'uuid/v4';
 
 // Toolkit Schema
 const toolkitSchema = [
-    {
-        id: uuid(),
-        key: 'checkbox',
-        name: 'Checkbox',
-        type: 'boolean',
-        default: false,
-        icon: 'fas fa-check-square',
-        required: true
-    },
-    {
-        id: uuid(),
-        key: 'email',
-        name: 'Email',
-        type: 'string',
-        format: 'email',
-        icon: 'fas fa-at',
-        required: true
-    },
-    {
-        id: uuid(),
-        key: 'date',
-        name: 'Fecha',
-        type: 'string',
-        format: 'date',
-        icon: 'fas fa-calendar-alt',
-        required: true
-    },
-    {
-        id: uuid(),
-        key: 'number',
-        name: 'Number',
-        type: 'number',
-        icon: 'fa-stack fa-1x number-icon',
-        required: true
-    },
-    {
-        id: uuid(),
-        key: 'paragraph',
-        name: 'Parrafo',
-        type: 'null',
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.Quod vero magnam quibusdam assumenda, voluptatem quo quos perferendis totam consequatur voluptates.',
-        icon: 'fas fa-paragraph',
-        required: true
-    },
-    {
-        id: uuid(),
-        key: 'radio',
-        name: 'Radio',
-        type: 'boolean',
-        default: false,
-        icon: 'fas fa-dot-circle',
-        required: true
-    },
-    {
-        id: uuid(),
-        key: 'select',
-        name: 'Select',
-        type: 'string',
-        enum: [
-            'opcion1',
-            'opcion2'
-        ],
-        enumNames: [
-            'Opcion 1',
-            'Opcion 2'
-        ],
-        icon: 'fas fa-angle-down',
-        required: true
-    },
-    {
-        id: uuid(),
-        key: 'textarea',
-        name: 'Textarea',
-        type: 'string',
-        icon: 'fa-stack textarea-icon',
-        required: true
-    },
-    {
-        id: uuid(),
-        key: 'text',
-        name: 'Texto',
-        type: 'string',
-        icon: 'fas fa-font',
-        required: true
-    },
+  {
+    id: uuid(),
+    key: 'checkbox',
+    name: 'Checkbox',
+    type: 'boolean',
+    default: false,
+    icon: 'fas fa-check-square',
+    required: true
+  },
+  {
+    id: uuid(),
+    key: 'email',
+    name: 'Email',
+    type: 'string',
+    format: 'email',
+    icon: 'fas fa-at',
+    required: true
+  },
+  {
+    id: uuid(),
+    key: 'date',
+    name: 'Fecha',
+    type: 'string',
+    format: 'date',
+    icon: 'fas fa-calendar-alt',
+    required: true
+  },
+  {
+    id: uuid(),
+    key: 'number',
+    name: 'Number',
+    type: 'number',
+    icon: 'fa-stack fa-1x number-icon',
+    required: true
+  },
+  {
+    id: uuid(),
+    key: 'paragraph',
+    name: 'Parrafo',
+    type: 'null',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit.Quod vero magnam quibusdam assumenda, voluptatem quo quos perferendis totam consequatur voluptates.',
+    icon: 'fas fa-paragraph',
+    required: true
+  },
+  {
+    id: uuid(),
+    key: 'radio',
+    name: 'Radio',
+    type: 'boolean',
+    default: false,
+    icon: 'fas fa-dot-circle',
+    required: true
+  },
+  {
+    id: uuid(),
+    key: 'select',
+    name: 'Select',
+    type: 'string',
+    enum: ['opcion1', 'opcion2'],
+    enumNames: ['Opcion 1', 'Opcion 2'],
+    icon: 'fas fa-angle-down',
+    required: true
+  },
+  {
+    id: uuid(),
+    key: 'textarea',
+    name: 'Textarea',
+    type: 'string',
+    icon: 'fa-stack textarea-icon',
+    required: true
+  },
+  {
+    id: uuid(),
+    key: 'text',
+    name: 'Texto',
+    type: 'string',
+    icon: 'fas fa-font',
+    required: true
+  }
 ];
 
 export default toolkitSchema;
