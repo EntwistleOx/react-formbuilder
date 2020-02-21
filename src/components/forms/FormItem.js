f66d92f843d6950641caaa3983864f1ab1225c20
Juan Díaz <juan.diaz.12069@gmail.com>
2020-02-21 19:02:27 -0300

    add component

---
+++ src/components/forms/FormItem.js
@@ -0,0 +1,38 @@
+import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { deleteForm } from '../../actions/form';
+import PropTypes from 'prop-types'
+
+const FormItem = ({ form, index, deleteForm }) => {
+    return (
+        <Fragment>
+            <tr>
+                <td>{index}</td>
+                <td>{form.schema.title}</td>
+                <td>{form.schema.description}</td>
+                <td>
+                    <Link to={`/form-view/${form.schema.idPrefix}`}>
+                        <i className="far fa-eye"></i>
+                    </Link>
+
+                    <Link to={`/form-edit/${form.schema.idPrefix}`}>
+                        <i className="fas fa-edit"></i>
+                    </Link>
+
+                    <Link to="#!">
+                        <i onClick={() => deleteForm(form.schema.idPrefix)} className="fas fa-trash-alt"></i>
+                    </Link>
+                </td>
+            </tr>
+        </Fragment>
+    )
+}
+
+FormItem.propTypes = {
+    form: PropTypes.object.isRequired,
+    index: PropTypes.number.isRequired,
+    deleteForm: PropTypes.func.isRequired,
+}
+
+export default connect(null, { deleteForm })(FormItem)
