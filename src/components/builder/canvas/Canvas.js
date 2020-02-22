289adbe8f1cc4f987faaf04a2ffdcbb3c24d347a
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-02-22 02:45:12 -0300

    add alert, on save can update and create

--- src/components/builder/canvas/Canvas.js
+++ src/components/builder/canvas/Canvas.js
@@ -1,86 +1,101 @@
 import React, { Fragment } from 'react';
-import CanvasElementTemplate from './CanvasElementTemplate';
-import { addForm, updateForm, clearForm } from '../../../actions/form';
-import { connect } from 'react-redux';
-import { Droppable } from 'react-beautiful-dnd';
-import Form from "react-jsonschema-form";
 import { Link, useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../../actions/alert';
+import { addForm, updateForm, clearForm, getForm } from '../../../actions/form';
+import CanvasElementTemplate from './CanvasElementTemplate';
+import { Droppable } from 'react-beautiful-dnd';
+import Form from 'react-jsonschema-form';
 import PropTypes from 'prop-types';
 
-// TODO:
-// onSave must add a form OR update form
+const Canvas = props => {
+  const { form, forms, addForm, updateForm, clearForm, setAlert } = props;
 
+  let history = useHistory();
 
-const Canvas = (props) => {
+  const onSave = () => {
+    const formExist = forms.find(item =>
+      item.schema.idPrefix === form.schema.idPrefix ? true : false
+    );
 
-    const { form, addForm, updateForm, clearForm, action } = props;
-
-    let history = useHistory();
-
-    const onSave = () => {
-        if (action === 'add') {
-            console.log('add')
-            addForm(form);
-        } else {
-            console.log('upodate')
-            updateForm(form);
-        }
-        clearForm()
-        history.push(`/forms`);
+    if (formExist) {
+      updateForm(form);
+      setAlert('Formulario Actualizado.', 'success');
+    } else {
+      addForm(form);
+      setAlert('Formulario Creado.', 'success');
     }
+    clearForm();
+    history.push(`/forms`);
+  };
 
-    return (
-        <Droppable key="Canvas" droppableId="Canvas" type="builder">
-            {(provided, snapshot) => (
-                <div id="formbuilder-canvas" ref={provided.innerRef}>
-                    <div className="form-wrap">
-                        {form.schema &&
-                            Object.keys(form.schema).length > 0 ?
-                            (
-                                <Form
-                                    schema={form.schema}
-                                    uiSchema={form.uiSchema}
-                                    // formData={form.formData}
-                                    FieldTemplate={CanvasElementTemplate}
-                                    disabled={true}
-                                >
-                                    {
-                                        Object.keys(form.schema.properties).length < 1 ? (
-                                            <Fragment>
-                                                <div className="notice">
-                                                    Arrastra los elementos del menu aca
-                                                </div>
-                                            </Fragment>
-                                        ) : ''
-                                    }
-                                    <hr />
-                                    <div className='form-buttons'>
-                                        <Link to='/formbuilder-render' className="btn btn-default">Probar</Link>
-                                        <button type="button" className="btn btn-success" onClick={onSave}>Guardar</button>
-                                    </div>
-                                </Form>
-                            )
-                            : (
-                                <Fragment>
-                                    <div className="notice">
-                                        Arrastra los elementos del menu aca
-                                    </div>
-                                </Fragment>
-                            )}
-                        {provided.placeholder}
-
+  return (
+    <Droppable key='Canvas' droppableId='Canvas' type='builder'>
+      {(provided, snapshot) => (
+        <div id='formbuilder-canvas' ref={provided.innerRef}>
+          <div className='form-wrap'>
+            {form.schema && Object.keys(form.schema).length > 0 ? (
+              <Form
+                schema={form.schema}
+                uiSchema={form.uiSchema}
+                // formData={form.formData}
+                FieldTemplate={CanvasElementTemplate}
+                disabled={true}
+              >
+                {Object.keys(form.schema.properties).length < 1 ? (
+                  <Fragment>
+                    <div className='notice'>
+                      Arrastra los elementos del menu aca
                     </div>
+                  </Fragment>
+                ) : (
+                  ''
+                )}
+                <hr />
+                <div className='form-buttons'>
+                  <Link to='/formbuilder-render' className='btn btn-default'>
+                    Probar
+                  </Link>
+                  <button
+                    type='button'
+                    className='btn btn-success'
+                    onClick={onSave}
+                  >
+                    Guardar
+                  </button>
                 </div>
+              </Form>
+            ) : (
+              <Fragment>
+                <div className='notice'>
+                  Arrastra los elementos del menu aca...
+                </div>
+              </Fragment>
             )}
-        </Droppable>
-    )
-}
+            {provided.placeholder}
+          </div>
+        </div>
+      )}
+    </Droppable>
+  );
+};
 
 Canvas.propTypes = {
-    form: PropTypes.object.isRequired,
-    addForm: PropTypes.func.isRequired,
-    updateForm: PropTypes.func.isRequired,
-    clearForm: PropTypes.func.isRequired,
-}
+  form: PropTypes.object.isRequired,
+  forms: PropTypes.array.isRequired,
+  addForm: PropTypes.func.isRequired,
+  updateForm: PropTypes.func.isRequired,
+  clearForm: PropTypes.func.isRequired
+};
 
-export default connect(null, { addForm, updateForm, clearForm })(Canvas);
+const mapStateToProps = state => ({
+  forms: state.forms
+});
+
+export default connect(mapStateToProps, {
+  addForm,
+  updateForm,
+  clearForm,
+  getForm,
+  setAlert
+})(Canvas);
