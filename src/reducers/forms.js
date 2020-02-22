46e47befc60cd65df3af4a2ef6d7604d0b822a0d
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-02-22 02:46:51 -0300

    update form

--- src/reducers/forms.js
+++ src/reducers/forms.js
@@ -1,32 +1,38 @@
 import {
-    ADD_FORM,
-    UPDATE_FORM,
-    GET_FORM,
-    DELETE_FORM,
-    FORM_ERROR
+  ADD_FORM,
+  UPDATE_FORM,
+  GET_FORM,
+  DELETE_FORM,
+  FORM_ERROR
 } from '../actions/types';
 
 const initial_state = [];
 
-export default function (state = initial_state, action) {
-    const { type, payload } = action;
+export default function(state = initial_state, action) {
+  const { type, payload } = action;
 
-    switch (type) {
-        case ADD_FORM:
-            return [...state, payload.formData];
-        case UPDATE_FORM:
-            console.log(payload)
-            return [...state, payload.formData];
-        case GET_FORM:
-            return state.find(form => form.schema.idPrefix === payload);
-        case DELETE_FORM:
-            return state.filter(form => form.schema.idPrefix !== payload);
-        case FORM_ERROR:
-            return {
-                ...state,
-                error: payload,
-            };
-        default:
-            return state;
-    }
+  switch (type) {
+    case ADD_FORM:
+      return [...state, payload.formData];
+    case UPDATE_FORM:
+      return state.map(form => {
+        if (form.schema.idPrefix === payload.formData.schema.idPrefix) {
+          return payload.formData;
+        }
+        return form;
+      });
+    case GET_FORM:
+      return state.find(form => {
+        if (form.schema.idPrefix === payload) return true;
+      });
+    case DELETE_FORM:
+      return state.filter(form => form.schema.idPrefix !== payload);
+    case FORM_ERROR:
+      return {
+        ...state,
+        error: payload
+      };
+    default:
+      return state;
+  }
 }
