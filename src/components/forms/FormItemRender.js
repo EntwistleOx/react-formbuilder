4ae11b127c3693b016052c547691cef09bb18fbf
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-02-22 02:48:43 -0300

    add loadform

--- src/components/forms/FormItemRender.js
+++ src/components/forms/FormItemRender.js
@@ -1,28 +1,31 @@
 import React, { useEffect, Fragment } from 'react';
 import { connect } from 'react-redux';
 import FormRender from '../render/FormRender';
+import { loadForm } from '../../actions/form';
 import PropTypes from 'prop-types';
 
 const FormItemRender = props => {
-    const id = props.match.params.id;
-    const { forms } = props;
+  const id = props.match.params.id;
+  const { forms, loadForm } = props;
 
-    const form = forms.find(form => form.schema.idPrefix === id)
+  const form = forms.find(form => form.schema.idPrefix === id);
+  loadForm(form);
 
-    return (
-        <Fragment>
-            <FormRender form={form} goBack={'/forms'} />
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <FormRender goBack={'/forms'} />
+    </Fragment>
+  );
+};
 
 FormItemRender.propTypes = {
-    forms: PropTypes.array.isRequired,
-    id: PropTypes.string,
-}
+  forms: PropTypes.array.isRequired,
+  id: PropTypes.string,
+  loadForm: PropTypes.func.isRequired
+};
 
-const mapStateToProps = (state) => ({
-    forms: state.forms
-})
+const mapStateToProps = state => ({
+  forms: state.forms
+});
 
-export default connect(mapStateToProps)(FormItemRender);
+export default connect(mapStateToProps, { loadForm })(FormItemRender);
