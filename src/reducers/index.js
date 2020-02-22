7bb8cca12a28dfed8cd2b756660da17ce7ad327f
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-02-22 02:43:47 -0300

    import  alert reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import forms from './forms';
 import form from './form';
+import alert from './alert';
 
 export default combineReducers({
-    forms,
-    form,
+  forms,
+  form,
+  alert
 });
