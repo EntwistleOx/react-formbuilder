324ac8e4c37fbdaef6201e074fdc77d41df80d5c
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-02-22 02:47:55 -0300

    typo

--- src/components/forms/Forms.js
+++ src/components/forms/Forms.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from 'react'
+import React, { useEffect, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { clearForm } from '../../actions/form';
@@ -6,68 +6,71 @@ import FormItem from './FormItem';
 import PropTypes from 'prop-types';
 
 // TODO:
-// edit form by id
 // view form by id
 
 const Forms = ({ forms, clearForm }) => {
+  useEffect(() => {
+    clearForm();
+  }, []);
 
-    useEffect(() => {
-        clearForm()
-    }, [])
+  return (
+    <Fragment>
+      <div
+        className='page-header'
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
+        <h1>Formularios</h1>
+        <Link to='/formbuilder' className='btn btn-default'>
+          Crear
+        </Link>
+      </div>
 
-    return (
-        <Fragment>
-            <div className="page-header" style={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center"
-            }}>
-                <h1 >Formularios</h1>
-                <Link to="/formbuilder" className="btn btn-default">Crear</Link>
-            </div>
-
-            {
-                forms.length > 0 ?
-                    (
-                        <table className="table table-striped table-hover  form-list">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Titulo</th>
-                                    <th>Descripcion</th>
-                                    <th>Acciones</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    forms.map((form, i) => (
-                                        <FormItem key={form.schema.idPrefix} form={form} index={i + 1} />
-                                    ))
-                                }
-                            </tbody>
-                        </table>
-                    ) : (
-                        <div className="well">
-                            <h4><strong>Aviso!</strong></h4>
-                            <p>No hay contenido disponible.
-                                <Link to="/formbuilder" className="alert-link">
-                                    {' '}Comienza a agregar formularios aca
-                                </Link>.
-                            </p>
-                        </div>
-                    )
-            }
-        </Fragment>
-    )
-}
+      {forms.length > 0 ? (
+        <table className='table table-striped table-hover  form-list'>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Titulo</th>
+              <th>Descripcion</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {forms.map((form, i) => (
+              <FormItem key={form.schema.idPrefix} form={form} index={i + 1} />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div className='well'>
+          <h4>
+            <strong>Aviso!</strong>
+          </h4>
+          <p>
+            No hay contenido disponible.
+            <Link to='/formbuilder' className='alert-link'>
+              {' '}
+              Comienza a agregar formularios aca
+            </Link>
+            .
+          </p>
+        </div>
+      )}
+    </Fragment>
+  );
+};
 
 Forms.propTypes = {
-    forms: PropTypes.array.isRequired,
-    clearForm: PropTypes.func.isRequired,
-}
+  forms: PropTypes.array.isRequired,
+  clearForm: PropTypes.func.isRequired
+};
 
-const mapStateToProps = (state) => ({
-    forms: state.forms,
-})
+const mapStateToProps = state => ({
+  forms: state.forms
+});
 
 export default connect(mapStateToProps, { clearForm })(Forms);
