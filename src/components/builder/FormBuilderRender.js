91a697d493156fb5b5b16fef3397a2b293235e5b
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-02-22 02:47:28 -0300

    connect to store

--- src/components/builder/FormBuilderRender.js
+++ src/components/builder/FormBuilderRender.js
@@ -1,23 +1,12 @@
 import React, { Fragment } from 'react';
-import { connect } from 'react-redux';
 import FormRender from '../render/FormRender';
-import PropTypes from 'prop-types';
 
-const FormBuilderRender = props => {
-    const { form } = props;
-    return (
-        <Fragment>
-            <FormRender form={form} goBack={'/formbuilder'} />
-        </Fragment>
-    )
+const FormBuilderRender = () => {
+  return (
+    <Fragment>
+      <FormRender goBack={'/formbuilder'} />
+    </Fragment>
+  );
 };
 
-FormBuilderRender.propTypes = {
-    form: PropTypes.object.isRequired,
-}
-
-const mapStateToProps = (state) => ({
-    form: state.form
-})
-
-export default connect(mapStateToProps)(FormBuilderRender);
+export default FormBuilderRender;
