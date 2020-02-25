8b11432aa484a75bc847b1a74d8e1c73a5b6fb7d
Juan Díaz <juan.diaz.12069@gmail.com>
2020-02-25 18:04:24 -0300

    add form id

--- src/App.js
+++ src/App.js
@@ -27,12 +27,8 @@ function App() {
           <Route exact path='/' component={Forms} />
           <Switch>
             <Route exact path='/formbuilder' component={FormBuilder} />
-            <Route
-              exact
-              path='/formbuilder-render'
-              component={FormBuilderRender}
-            />
-            <Route exact path='/formbuilder/:id' component={EditFormElement} />
+            <Route exact path='/formbuilder/:formId/:id' component={EditFormElement} />
+            <Route exact path='/formbuilder-render' component={FormBuilderRender} />
             <Route exact path='/forms' component={Forms} />
             <Route exact path='/form-view/:id' component={FormItemRender} />
             <Route exact path='/form-edit/:id' component={FormEdit} />
