f5b4d2d5b713620d6c2495811fe8ae79dfae0005
Juan Díaz <juan.diaz.12069@gmail.com>
2020-02-25 18:03:37 -0300

    add well bootstrap class

--- src/components/builder/toolkit/Toolkit.js
+++ src/components/builder/toolkit/Toolkit.js
@@ -19,6 +19,7 @@ const Toolkit = ({ toolkitSchema, createForm }) => {
             {(provided, snapshot) => (
 
                 <div id="formbuilder-toolkit"
+                    className="well"
                     ref={provided.innerRef}
                 >
                     <a
