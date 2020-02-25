c68dced5d002750b19160b9b9480eb6371a8155f
Juan Díaz <juan.diaz.12069@gmail.com>
2020-02-25 18:08:05 -0300

    fix useeffect and draggable, add form id in dispatch functions

--- src/components/builder/canvas/CanvasElementTemplate.js
+++ src/components/builder/canvas/CanvasElementTemplate.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { deleteElement, deleteUiOrder, deleteWidget } from '../../../actions/form';
+import { deleteElement, deleteUiOrder, deleteWidget, deleteFormElement } from '../../../actions/form';
 import { Draggable } from 'react-beautiful-dnd';
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types';
 
 // TODO:
 // Move inline css to styles file
+// fix delete icon position in form title
+// show only form title
 
 const CanvasElementTemplate = (
     props
@@ -22,6 +24,7 @@ const CanvasElementTemplate = (
         deleteElement,
         deleteUiOrder,
         deleteWidget,
+        deleteFormElement,
         form
     } = props
 
@@ -29,64 +32,70 @@ const CanvasElementTemplate = (
     const element = id.split('_');
     const elementId = element[1];
 
-    console.log('aers', props.schema.idPrefix)
-    console.log('aers', form)
-    // console.log('aers', id)
-
     const [order, setOrder] = useState(0);
 
-    // useEffect(() => {
-    //     // setOrder(uiState.findIndex(el => el === elementId))
-    // }, [uiState, elementId])
+    useEffect(() => {
+        form.forEach((form) => {
+            const result = form.uiSchema["ui:order"].findIndex(el => el === elementId)
+            if (result !== -1) setOrder(result)
+        })
+    }, [form, elementId])
 
     const onDelete = () => {
-        deleteElement(elementId);
-        deleteUiOrder(elementId);
-        deleteWidget(elementId);
+        deleteElement(elementId, props.formContext.idPrefix);
+        deleteUiOrder(elementId, props.formContext.idPrefix);
+        deleteWidget(elementId, props.formContext.idPrefix);
     }
 
-    console.log(form)
     return (
-        form.length ?
-            form.map((form, index) => {
-                console.log(form, index)
-                return (
-                    <Draggable
-                        key={id}
-                        draggableId={id}
-                        index={index}
-                    >
-                        {(provided, snapshot) => (
-                            <div
-                                className={classNames}
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                            >
-                                <div style={{ display: 'flex', justifyContent: 'space-between', userSelect: 'none' }}>
-                                    <label htmlFor={id}>{label} {required ? "*" : null}</label>
-                                    <div>
-                                        <Link to={`/formbuilder/${elementId}`}>
-                                            <i className="fas fa-edit"></i>
-                                        </Link>
+        (id === 'root') ? (
+            <Fragment>
+                <div style={{
+                    position: 'relative',
+                    marginLeft: '94%',
+                    top: '26px',
+                    marginTop: '-2rem'
+                }}>
+                    <div style={{ display: 'flex', justifyContent: 'space-between', userSelect: 'none' }}>
+                        <Link to={`/formbuilder/${props.schema.idPrefix}/${props.schema.idPrefix}`}>
+                            <i className="fas fa-edit"></i>
+                        </Link>
+                        <Link to="#!">
+                            <i onClick={() => deleteFormElement(props.schema.idPrefix)} className="fas fa-trash-alt"></i>
+                        </Link>
+                    </div>
+                </div>
+                {children}
+            </Fragment>
+        ) : (
+                <Draggable index={order} draggableId={id}>
+                    {(provided, snapshot) => (
+                        <div
+                            className={classNames}
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                        >
+                            <div style={{ display: 'flex', justifyContent: 'space-between', userSelect: 'none' }}>
+                                <label htmlFor={id}>{label} {required ? "*" : null}</label>
+                                <div>
+                                    <Link to={`/formbuilder/${props.formContext.idPrefix}/${elementId}`}>
+                                        <i className="fas fa-edit"></i>
+                                    </Link>
 
-                                        <Link to="#!">
-                                            <i onClick={onDelete} className="fas fa-trash-alt"></i>
-                                        </Link>
-                                        <i {...provided.dragHandleProps} className="fas fa-arrows-alt"></i>
-                                    </div>
+                                    <Link to="#!">
+                                        <i onClick={onDelete} className="fas fa-trash-alt"></i>
+                                    </Link>
+                                    <i {...provided.dragHandleProps} className="fas fa-arrows-alt"></i>
                                 </div>
-                                {children}
-                                {description && description}
                             </div>
-                        )
-                        }
-                    </Draggable >
-                )
-            }
-
-            ) : ''
+                            {children}
+                            {description && description}
+                        </div>
+                    )
+                    }
+                </Draggable >
+            )
     )
-
 };
 
 CanvasElementTemplate.propTypes = {
@@ -101,6 +110,8 @@ CanvasElementTemplate.propTypes = {
     deleteElement: PropTypes.func.isRequired,
     deleteUiOrder: PropTypes.func.isRequired,
     deleteWidget: PropTypes.func.isRequired,
+    deleteFormElement: PropTypes.func.isRequired,
+    form: PropTypes.array.isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -111,7 +122,7 @@ const mapStateToProps = state => ({
 
 // Add a wrapped to return a class component to prevent warning
 // Source: https://github.com/rjsf-team/react-jsonschema-form/issues/1309
-var ReduxWrapped = connect(mapStateToProps, { deleteElement, deleteUiOrder, deleteWidget })(CanvasElementTemplate);
+var ReduxWrapped = connect(mapStateToProps, { deleteElement, deleteUiOrder, deleteWidget, deleteFormElement })(CanvasElementTemplate);
 class CanvasElementTemplateWrapper extends React.Component {
     render() {
         return (
