08c786ae74a0390b4cf93678b432f89edb56a6b6
Juan Díaz <juan.diaz.12069@gmail.com>
2020-02-25 18:10:17 -0300

    add wizard function, in progress

--- src/components/render/FormRender.js
+++ src/components/render/FormRender.js
@@ -3,130 +3,168 @@ import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
 import Form from 'react-jsonschema-form';
-import JSONPretty from 'react-json-prettify';
 import PropTypes from 'prop-types';
 
-const FormRender = ({ form, goBack, setAlert }) => {
-  const [renderState, setRenderState] = useState({});
+// TODO:
+// Validations
+// useeffect 
+// render from forms list
+// 
 
-  useEffect(() => {
-    setRenderState(form);
-  }, []);
+const FormRender = ({ forms, goBack, setAlert }) => {
+  // const [renderState, setRenderState] = useState({});
 
-  function transformErrors(errors) {
-    return errors.map(error => {
-      if (error.name === 'required') {
-        error.message = 'El campo es requerido.';
+  const [stepsState, setStepsState] = useState({ step: 1, formData: {} })
+
+  // useEffect(() => {
+  //   setRenderState(forms);
+  // }, []);
+
+  // function transformErrors(errors) {
+  //   return errors.map(error => {
+  //     if (error.name === 'required') {
+  //       error.message = 'El campo es requerido.';
+  //     }
+  //     return error;
+  //   });
+  // }
+
+  // const validate = (formData, errors) => {
+  //   // RUT validos
+  //   //     10864629-2
+  //   //     16158088-0
+  //   //     23023518-k
+  //   //     6709127-2
+
+  //   // RUT invalidos
+  //   //     10864629-1
+  //   //     11726111-6
+  //   //     16003145-0
+  //   //     16158088-1
+
+  //   Object.keys(formData).forEach(function(item) {
+  //     const key = item;
+  //     const val = formData[item];
+
+  //     if (renderState && renderState.schema.properties[item].key === 'rut') {
+  //       if (val === undefined) {
+  //         return;
+  //       }
+
+  //       if (!val || val.trim().length < 3) {
+  //         errors[key].addError('Rut invalido.');
+  //         return;
+  //       }
+
+  //       const rutLimpio = val.replace(/[^0-9kK-]/g, '');
+
+  //       if (rutLimpio.length < 3) {
+  //         errors[key].addError('Rut invalido.');
+  //         return;
+  //       }
+
+  //       const split = rutLimpio.split('-');
+
+  //       if (split.length !== 2) {
+  //         errors[key].addError('Rut invalido.');
+  //         return;
+  //       }
+
+  //       const num = parseInt(split[0], 10);
+  //       const dgv = split[1];
+
+  //       function digitoVerificadorCalc() {
+  //         const cuerpo = `${num}`;
+  //         // Calcular Dígito Verificador
+  //         let suma = 0;
+  //         let multiplo = 2;
+
+  //         // Para cada dígito del Cuerpo
+  //         for (let i = 1; i <= cuerpo.length; i++) {
+  //           // Obtener su Producto con el Múltiplo Correspondiente
+  //           const index = multiplo * cuerpo.charAt(cuerpo.length - i);
+
+  //           // Sumar al Contador General
+  //           suma += index;
+
+  //           // Consolidar Múltiplo dentro del rango [2,7]
+  //           if (multiplo < 7) {
+  //             multiplo += 1;
+  //           } else {
+  //             multiplo = 2;
+  //           }
+  //         }
+
+  //         // Calcular Dígito Verificador en base al Módulo 11
+  //         const dvEsperado = 11 - (suma % 11);
+  //         if (dvEsperado === 10) return 'k';
+  //         if (dvEsperado === 11) return '0';
+  //         return dvEsperado;
+  //       }
+
+  //       let dvCalc = digitoVerificadorCalc();
+
+  //       console.log(dvCalc, dgv);
+  //       if (dvCalc == dgv) {
+  //         return errors;
+  //       } else {
+  //         errors[key].addError('Rut invalido.');
+  //       }
+  //     }
+  //   });
+  //   return errors;
+  // };
+
+  // TODO:
+  // aca se debe validar en cada step
+  // y al llegar al final mostrar los resultadps
+  const onSubmit = ({ formData }) => {
+    let index = 0
+    for (index; index <= stepsState.step; index++) {
+
+      if (stepsState.step === index) {
+        setStepsState({
+          step: index + 1,
+          formData: {
+            ...stepsState.formData,
+            ...formData
+          },
+        });
+      } else {
+        alert("You submitted " + JSON.stringify(formData, null, 2));
+        setAlert('Formulario Valido.', 'success');
+        console.log(formData);
       }
-      return error;
-    });
+    }
+
+    // if (forms.length < index) {
+    //   alert("You submitted " + JSON.stringify(formData, null, 2));
+    //   setAlert('Formulario Valido.', 'success');
+    //   console.log(formData);
+    // }
+  };
+
+  function getSteps() {
+    let step = forms.find((step, index) => stepsState.step === index + 1)
+    if (step === undefined) step = {}
+    return step
   }
 
-  const validate = (formData, errors) => {
-    // RUT validos
-    //     10864629-2
-    //     16158088-0
-    //     23023518-k
-    //     6709127-2
-
-    // RUT invalidos
-    //     10864629-1
-    //     11726111-6
-    //     16003145-0
-    //     16158088-1
-
-    Object.keys(formData).forEach(function(item) {
-      const key = item;
-      const val = formData[item];
-
-      if (renderState && renderState.schema.properties[item].key === 'rut') {
-        if (val === undefined) {
-          return;
-        }
-
-        if (!val || val.trim().length < 3) {
-          errors[key].addError('Rut invalido.');
-          return;
-        }
-
-        const rutLimpio = val.replace(/[^0-9kK-]/g, '');
-
-        if (rutLimpio.length < 3) {
-          errors[key].addError('Rut invalido.');
-          return;
-        }
-
-        const split = rutLimpio.split('-');
-
-        if (split.length !== 2) {
-          errors[key].addError('Rut invalido.');
-          return;
-        }
-
-        const num = parseInt(split[0], 10);
-        const dgv = split[1];
-
-        function digitoVerificadorCalc() {
-          const cuerpo = `${num}`;
-          // Calcular Dígito Verificador
-          let suma = 0;
-          let multiplo = 2;
-
-          // Para cada dígito del Cuerpo
-          for (let i = 1; i <= cuerpo.length; i++) {
-            // Obtener su Producto con el Múltiplo Correspondiente
-            const index = multiplo * cuerpo.charAt(cuerpo.length - i);
-
-            // Sumar al Contador General
-            suma += index;
-
-            // Consolidar Múltiplo dentro del rango [2,7]
-            if (multiplo < 7) {
-              multiplo += 1;
-            } else {
-              multiplo = 2;
-            }
-          }
-
-          // Calcular Dígito Verificador en base al Módulo 11
-          const dvEsperado = 11 - (suma % 11);
-          if (dvEsperado === 10) return 'k';
-          if (dvEsperado === 11) return '0';
-          return dvEsperado;
-        }
-
-        let dvCalc = digitoVerificadorCalc();
-
-        console.log(dvCalc, dgv);
-        if (dvCalc == dgv) {
-          return errors;
-        } else {
-          errors[key].addError('Rut invalido.');
-        }
-      }
-    });
-    return errors;
-  };
-
-  const onSubmit = formData => {
-    setAlert('Formulario Valido.', 'success');
-    console.log(formData);
-  };
-
-  if ((Object.keys(form) && Object.keys(form).length < 1) || form.error) {
+  if ((Object.keys(forms) && Object.keys(forms).length < 1) || forms.error) {
     return <Redirect to='/' />;
   }
 
+  console.log(forms)
   return (
     <Fragment>
       <Form
-        schema={renderState.schema ? renderState.schema : {}}
-        uiSchema={renderState.uiSchema ? renderState.uiSchema : {}}
+        schema={forms ? getSteps().schema : {}}
+        // uiSchema={forms ? renderState.uiSchema : {}}
         onSubmit={onSubmit}
-        // formData={formSchema.formData}
+        formData={stepsState.formData}
         // liveValidate={true}
-        transformErrors={transformErrors}
-        validate={validate}
+        // transformErrors={transformErrors}
+        // validate={validate}
         showErrorList={false}
         noHtml5Validate={true}
       >
@@ -145,12 +183,12 @@ const FormRender = ({ form, goBack, setAlert }) => {
 };
 
 FormRender.propTypes = {
-  form: PropTypes.object.isRequired,
+  forms: PropTypes.array.isRequired,
   goBack: PropTypes.string.isRequired
 };
 
 const mapStateToProps = state => ({
-  form: state.form
+  forms: state.form
 });
 
 export default connect(mapStateToProps, { setAlert })(FormRender);
