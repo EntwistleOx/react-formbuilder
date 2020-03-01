13eacf3dccd78cfe6cdbd4aa525d76fec2a5b464
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-03-01 03:12:06 -0300

    add condition on onChange, add disabled, placeholder props

--- src/components/custom-widgets/EmailAutocomplete.js
+++ src/components/custom-widgets/EmailAutocomplete.js
@@ -1,136 +1,150 @@
 import React, { useState, useEffect, useRef, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
-const EmailAutocomplete = (props) => {
+const EmailAutocomplete = props => {
+  const suggestions = [
+    '2call.cl',
+    'gmail.com',
+    'hotmail.com',
+    'outlook.com',
+    'yahoo.com'
+  ];
 
-    console.log(props)
+  const [state, setState] = useState({
+    // The active selection's index
+    activeSuggestion: 0,
+    // The suggestions that match the user's input
+    filteredSuggestions: [],
+    // Whether or not the suggestion list is shown
+    showSuggestions: false,
+    // What the user has entered
+    userInput: '',
+    dropdownOpen: ''
+  });
 
-    const suggestions = [
-        '2call.cl',
-        'gmail.com',
-        'hotmail.com',
-        'outlook.com',
-        'yahoo.com'
-    ]
+  // To reference outside elements
+  const node = useRef();
 
-    const [state, setState] = useState({
-        // The active selection's index
+  // To manage handleClick to close dropdown
+  useEffect(() => {
+    // add when mounted
+    document.addEventListener('mousedown', handleClick);
+    // return function to be called when unmounted
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  }, []);
+
+  const onChange = (e, fromClick) => {
+    const input = e;
+
+    if (!fromClick) {
+      // Pattern: .*@.*
+      let reg = new RegExp(
+        input
+          .split('')
+          .join('\\w*\\s*\\w*')
+          .replace(/.*@.*/, ''),
+        'i'
+      );
+      let filteredSuggestions = suggestions.filter(function(sugestion) {
+        if (sugestion.match(reg)) {
+          return sugestion;
+        }
+      });
+      setState({
         activeSuggestion: 0,
-        // The suggestions that match the user's input
+        filteredSuggestions,
+        showSuggestions: true,
+        userInput: input,
+        dropdownOpen: 'open'
+      });
+
+      return props.onChange(state.userInput);
+    } else {
+      setState({
+        activeSuggestion: 0,
         filteredSuggestions: [],
-        // Whether or not the suggestion list is shown
-        showSuggestions: false,
-        // What the user has entered
-        userInput: props.formData,
-        dropdownOpen: ''
+        showSuggestions: true,
+        userInput: input,
+        dropdownOpen: 'open'
+      });
+      return props.onChange(input);
+    }
+  };
+
+  // Event fired when the user clicks on a suggestion
+  const onClick = e => {
+    // Update the user input and reset the rest of the state
+
+    const email = state.userInput + e.currentTarget.innerText;
+    onChange(email, true);
+
+    setState({
+      activeSuggestion: 0,
+      filteredSuggestions: [],
+      showSuggestions: false,
+      userInput: state.userInput + e.currentTarget.innerText,
+      dropdownOpen: ''
     });
+  };
 
-    // To reference outside elements
-    const node = useRef();
-
-    // To manage handleClick to close dropdown
-    useEffect(() => {
-        // add when mounted
-        document.addEventListener("mousedown", handleClick);
-        // return function to be called when unmounted
-        return () => {
-            document.removeEventListener("mousedown", handleClick);
-        };
-    }, []);
-
-    const onChange = (e) => {
-        const input = e.target.value;
-
-        // // // // Pattern: .*@.*
-        let reg = new RegExp(input.split('').join('\\w*\\s*\\w*').replace(/.*@.*/, ""), 'i');
-        let filteredSuggestions = suggestions.filter(function (sugestion) {
-            if (sugestion.match(reg)) {
-                return sugestion;
-            }
-        });
-        setState({
-            activeSuggestion: 0,
-            filteredSuggestions,
-            showSuggestions: true,
-            userInput: input,
-            dropdownOpen: 'open'
-        });
-
-        // retorna callback
-        return props.onChange(state.userInput);
+  // Close Dropdown
+  const handleClick = e => {
+    if (node.current.contains(e.target)) {
+      // inside click
+      return;
     }
 
-    // Event fired when the user clicks on a suggestion
-    const onClick = e => {
-        // Update the user input and reset the rest of the state
-        setState({
-            activeSuggestion: 0,
-            filteredSuggestions: [],
-            showSuggestions: false,
-            userInput: state.userInput + e.currentTarget.innerText,
-            dropdownOpen: ''
-        });
-    };
+    // outside click and setState
+    setState({
+      ...state,
+      activeSuggestion: 0,
+      filteredSuggestions: [],
+      showSuggestions: false,
+      userInput: props.value,
+      dropdownOpen: ''
+    });
+  };
 
-    // Close Dropdown
-    const handleClick = e => {
+  let suggestionsListComponent;
 
-        if (node.current.contains(e.target)) {
-            // inside click
-            return;
-        }
-
-        // outside click and setState
-        setState({
-            ...state,
-            activeSuggestion: 0,
-            filteredSuggestions: [],
-            showSuggestions: false,
-            userInput: state.userInput,
-            dropdownOpen: ''
-        });
-    }
-
-    let suggestionsListComponent;
-
-    if (state.showSuggestions && state.userInput) {
-        if (state.filteredSuggestions.length) {
-            suggestionsListComponent = (
-                <Fragment>
-                    <div className={`dropdown ${state.dropdownOpen}`}>
-                        <ul className="dropdown-menu" role="menu">
-                            {state.filteredSuggestions.map((suggestion, index) => {
-                                return (
-                                    <li
-                                        key={suggestion}
-                                        onClick={onClick}
-                                    >
-                                        <Link to="#!">
-                                            {suggestion}
-                                        </Link>
-                                    </li>
-                                );
-                            })}
-                        </ul>
-                    </div>
-                </Fragment>
-            );
-        }
-    };
-
-    return (
+  if (state.showSuggestions && state.userInput) {
+    if (state.filteredSuggestions.length) {
+      suggestionsListComponent = (
         <Fragment>
-            <div className="form-group" ref={node}>
-                <input
-                    type="email"
-                    className="form-control"
-                    onChange={onChange}
-                    value={state.userInput}
-                />
-                {suggestionsListComponent}
-            </div>
+          <div className={`dropdown ${state.dropdownOpen}`}>
+            <ul className='dropdown-menu' role='menu'>
+              {state.filteredSuggestions.map((suggestion, index) => {
+                return (
+                  <li key={suggestion} onClick={onClick}>
+                    <Link to='#!'>{suggestion}</Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </Fragment>
-    )
+      );
+    }
+  }
+
+  return (
+    <Fragment>
+      <div className='form-group' ref={node}>
+        <input
+          type='email'
+          className='form-control'
+          value={state.userInput}
+          placeholder={props.placeholder}
+          disabled={props.disabled}
+          onChange={e => onChange(e.target.value, false)}
+          onKeyUp={e => onChange(e.target.value, false)}
+        />
+        {suggestionsListComponent}
+      </div>
+    </Fragment>
+  );
 };
 
 export default EmailAutocomplete;
