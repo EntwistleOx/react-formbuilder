4c9b7b6db638cd9c6d1d89a3ee972613f0903f84
Juan Díaz <juan.diaz.12069@gmail.com>
2020-03-09 18:30:33 -0300

    test component

--- src/components/test-components/BuilderTestSchema.js
+++ src/components/test-components/BuilderTestSchema.js
@@ -13,56 +13,56 @@ const schema = {
     type: 'object',
     // required: ['oferta', 'motivo', 'calle', 'numero', 'comuna'],
     properties: {
-        nombre: { type: "string" },
-        edad: { type: "number" },
-        parrafo: {
-            "title": "Parrafo",
-            "type": "null",
-            "key": "paragraph",
-            "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Quod vero magnam quibusdam assumenda, voluptatem quo quos perferendis totam consequatur voluptates."
-        },
-        oferta: {
-            title: "Acepta La Oferta?",
-            type: "string",
-            key: "select",
-            enum: [
-                "Acepta",
-                "No Acepta",
-                "Lo Pensara"
-            ],
-            enumVal: [
-                "acepta",
-                "no-acepta",
-                "lo-pensara"
-            ],
-            default: "Lo Pensara"
-        },
-        motivo: {
-            title: "Motivo no acepta?",
-            type: "string",
-            enum: [
-                "Sin Trabajo",
-                "No Confia en Llamado",
-                "CAE Muy Alto"
-            ],
-            enumVal: [
-                "sin-trabajo",
-                "no-confia",
-                "cae-muy-alto"
-            ],
-        },
-        calle: { type: "string" },
-        numero: { type: "number" },
-        comuna: { type: "string" }
+        // nombre: { type: "string" },
+        // edad: { type: "number" },
+        // parrafo: {
+        //     "title": "Parrafo",
+        //     "type": "null",
+        //     "key": "paragraph",
+        //     "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Quod vero magnam quibusdam assumenda, voluptatem quo quos perferendis totam consequatur voluptates."
+        // },
+        // oferta: {
+        //     title: "Acepta La Oferta?",
+        //     type: "string",
+        //     key: "select",
+        //     enum: [
+        //         "Acepta",
+        //         "No Acepta",
+        //         "Lo Pensara"
+        //     ],
+        //     enumVal: [
+        //         "acepta",
+        //         "no-acepta",
+        //         "lo-pensara"
+        //     ],
+        //     default: "Lo Pensara"
+        // },
+        // motivo: {
+        //     title: "Motivo no acepta?",
+        //     type: "string",
+        //     enum: [
+        //         "Sin Trabajo",
+        //         "No Confia en Llamado",
+        //         "CAE Muy Alto"
+        //     ],
+        //     enumVal: [
+        //         "sin-trabajo",
+        //         "no-confia",
+        //         "cae-muy-alto"
+        //     ],
+        // },
+        // calle: { type: "string" },
+        // numero: { type: "number" },
+        // comuna: { type: "string" }
     },
 }
 
 const groups = [
     {
-        "Paso-0": ["nombre", "edad"],
-        "Paso-1": ["parrafo", "oferta", /*"motivo", "calle", "numero", "comuna"*/],
-        "Paso-2": ["motivo"],
-        "Paso-3": ["calle", "numero", "comuna"],
+        "Paso-0": [/*"nombre", "edad"*/],
+        "Paso-1": [/*"parrafo", "oferta"*/],
+        // "Paso-2": ["motivo"],
+        // "Paso-3": ["calle", "numero", "comuna"],
         "ui:template": "well",
     }
 ];
@@ -94,7 +94,7 @@ const TestSchema = () => {
 
             <SchemaViewer form={schema} />
             {/* <SchemaViewer form={uiSchema} /> */}
-        </div>
+        </div >
     )
 }
 
