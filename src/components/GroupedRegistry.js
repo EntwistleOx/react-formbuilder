b4e0483b175be811e3f964c5a2287d814a165fa4
Juan Díaz <juan.diaz.12069@gmail.com>
2020-03-09 18:29:19 -0300

    grouped template component

---
+++ src/components/GroupedRegistry.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import { Tabs, Tab, Table, Well, Button } from "react-bootstrap";
+import { Droppable } from 'react-beautiful-dnd';
+
+export const GroupTemplates = {
+    hideshow: props => (
+        <div style={{ background: "grey" }}>
+            Hide/show{props.properties.map(p => p.children)}
+        </div>
+    ),
+    grid: props => (
+        <Table striped bordered condensed hover>
+            <tbody>
+                {props.properties.map(p => (
+                    <tr>
+                        <td>{p.name}</td>
+                        {p.children.map(c => (
+                            <td>{c}</td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+    ),
+    table: props => (
+        <Table striped bordered condensed hover>
+            <tbody>
+                {props.properties.map(p => (
+                    <tr>
+                        <th>{p.name}</th>
+                        <td>{p.children}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+    ),
+    well: props => props.properties.map(p => {
+        console.log(props)
+        return (
+            <Well key={p.key}>
+                <Droppable
+                    key={p.key}
+                    droppableId={p.key}
+                    type='builder'
+                >
+                    {(provided, snapshot) => (
+                        <div ref={provided.innerRef}>
+                            {p.name}
+                            {p.children}
+
+                            {Object.keys(p.children).length === 0 && (
+                                <div className='notice'>
+                                    Arrastra los elementos del menu aca
+                               </div>
+                            )}
+
+                            <div className='form-buttons'>
+                                <Button className='btn btn-primary btn-sm'>Back</Button>
+                                <Button className='btn btn-primary btn-sm'>Next</Button>
+                            </div>
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </Well>
+        )
+    }
+    ),
+    tabs: props => {
+        return (
+            <Tabs defaultActiveKey={0} id="uncontrolled-tab-example">
+                {props.properties.map((p, idx) => (
+                    <Tab eventKey={idx} title={p.name}>
+                        {p.children}
+                    </Tab>
+                ))}
+            </Tabs>
+        );
+    },
+    controlledTabs: ControlledTabs
+};
+
+function ControlledTabs(props) {
+    const [active, setActive] = useState(0);
+
+    let count = -1
+    return (
+        <Tabs activeKey={active} onSelect={setActive} id="controlled-tab">
+            {props.properties.map((p, idx) => {
+
+                // true if all childs are undefined
+                const check = p.children.every(child => child === undefined)
+                if (!check) count = count + 1 // add 1 to counter
+
+                if (p.children[0] !== undefined) {
+                    const notLast = props.properties.length - idx > 1;
+                    const isLast = !notLast;
+                    const notFirst = idx >= 1 && props.properties.length > 1;
+                    return (
+                        <Tab key={count} eventKey={count} title={p.name}>
+                            {p.children}
+                            {/* {notFirst && ( */}
+                            <Button onClick={() => setActive(count - 1)}>Back</Button>
+                            {/* )}
+                            {notLast && ( */}
+                            <Button onClick={() => setActive(count)}>Next</Button>
+                            {/* )} */}
+                            {/* {isLast && <Button type="submit">Submit</Button>} */}
+                        </Tab>
+                    );
+                } else {
+                    return
+                }
+
+
+            })}
+        </Tabs>
+    );
+}
