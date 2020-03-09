4ec57655f2acde4f9afc71e44a35f7590064784f
Juan Díaz <juan.diaz.12069@gmail.com>
2020-03-09 18:28:58 -0300

    field template component

---
+++ src/components/FieldTemplate.js
@@ -0,0 +1,51 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+const FieldTemplate = props => {
+  const {
+    id,
+    label,
+    required,
+    description,
+    children,
+  } = props;
+
+  const element = id.split('_');
+  const elementId = element[1];
+
+  return (
+    <Fragment>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          userSelect: 'none'
+        }}
+      >
+        {id !== 'root' && (
+          <Fragment>
+            <label>
+              {label} {required ? '*' : null}
+            </label>
+            <div>
+              <Link
+                to={`/formbuilder/${'formId'}/${'idPrefix'}/${elementId}`}
+              >
+                <i className='fas fa-edit'></i>
+              </Link>
+              <Link to='#!'>
+                <i className='fas fa-trash-alt'></i>
+              </Link>
+            </div>
+          </Fragment>
+        )}
+      </div>
+
+      {children}
+
+      {id !== 'root' && description}
+    </Fragment>
+  );
+};
+
+export default FieldTemplate;
