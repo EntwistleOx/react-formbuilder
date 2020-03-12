304aa2b601429221640b25152ed59799fa11185f
Juan Díaz <juan.diaz.12069@gmail.com>
2020-03-12 15:47:03 -0300

    disabled next btn if fields are empty

--- src/components/GroupedRegistry.js
+++ src/components/GroupedRegistry.js
@@ -1,6 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import { Tabs, Tab, Table, Well, Button } from "react-bootstrap";
 import { Droppable } from 'react-beautiful-dnd';
+
+
+// TODO:
+// // disabled next tabs, enabled past tabs
 
 export const GroupTemplates = {
     hideshow: props => (
@@ -36,7 +40,7 @@ export const GroupTemplates = {
     ),
     well: props => props.properties.map(p => {
         return (
-            <Well key={p.key}>
+            < Well key={p.key} >
                 <Droppable
                     key={p.key}
                     droppableId={p.key}
@@ -56,7 +60,7 @@ export const GroupTemplates = {
                         </div>
                     )}
                 </Droppable>
-            </Well>
+            </Well >
         )
     }
     ),
@@ -77,37 +81,72 @@ export const GroupTemplates = {
 function ControlledTabs(props) {
     const [active, setActive] = useState(0);
 
-    let count = -1
     return (
         <Tabs activeKey={active} onSelect={setActive} id="controlled-tab">
             {props.properties.map((p, idx) => {
 
-                // true if all childs are undefined
-                const check = p.children.every(child => child === undefined)
-                if (!check) count = count + 1 // add 1 to counter
+                // true if every formdata child is undefined
+                const hasEmptyFields = p.children.every(child => child.props.formData !== undefined)
+                const hasErrors = p.children.every(child => Object.keys(child.props.errorSchema).length > 0)
 
-                if (p.children[0] !== undefined) {
-                    const notLast = props.properties.length - idx > 1;
-                    const isLast = !notLast;
-                    const notFirst = idx >= 1 && props.properties.length > 1;
-                    return (
-                        <Tab key={count} eventKey={count} title={p.name}>
-                            {p.children}
-                            {/* {notFirst && ( */}
-                            <Button onClick={() => setActive(count - 1)}>Back</Button>
-                            {/* )}
-                            {notLast && ( */}
-                            <Button onClick={() => setActive(count)}>Next</Button>
-                            {/* )} */}
-                            {/* {isLast && <Button type="submit">Submit</Button>} */}
-                        </Tab>
-                    );
-                } else {
-                    return
-                }
+                // console.log(hasErrors)
 
+                const notLast = props.properties.length - idx > 1;
+                const isLast = !notLast;
+                const notFirst = idx >= 1 && props.properties.length > 1;
 
+                const isNextTab = active < props.properties.length ? true : false;
+
+                console.log(active, props.properties.length)
+                return (
+                    <Tab key={idx} eventKey={idx} title={p.name} disabled={isNextTab} style={{ color: '#ff0039', borderColor: '#f0005e' }}>
+                        {p.children}
+                        <div className='form-buttons'>
+                            {notFirst && (
+                                <Button className='btn-primary' onClick={() => setActive(idx - 1)}>Anterior</Button>
+                            )}
+                            {notLast && (
+                                <Fragment><div></div><Button className='btn-primary' onClick={() => setActive(idx + 1)} disabled={!hasEmptyFields}>Siguiente</Button></Fragment>
+                            )}
+                            {isLast && <Fragment><div></div><Button className='btn-success' type="submit" disabled={!hasEmptyFields}>Validar</Button></Fragment>}
+                        </div>
+                    </Tab>
+                );
             })}
-        </Tabs>
-    );
+        </Tabs >
+    )
+    // let count = -1
+    // return (
+    //     <Tabs activeKey={active} onSelect={setActive} id="controlled-tab">
+    //         {props.properties.map((p, idx) => {
+
+    //             // true if all childs are undefined
+    //             const check = p.children.every(child => child === undefined)
+    //             if (!check) count = count + 1 // add 1 to counter
+
+    //             if (p.children[0] !== undefined) {
+    //                 const notLast = props.properties.length - idx > 1;
+    //                 const isLast = !notLast;
+    //                 const notFirst = idx >= 1 && props.properties.length > 1;
+
+    //                 return (
+    //                     <Tab key={count} eventKey={count} title={p.name}>
+    //                         {p.children}
+    //                         <div className='form-buttons'>
+    //                             {notFirst && (
+    //                                 <Button className='btn-primary' onClick={() => setActive(count - 1)}>Anterior</Button>
+    //                             )}
+    //                             {notLast && (
+    //                                 <Button className='btn-primary' onClick={() => setActive(count)}>Siguiente</Button>
+    //                             )}
+    //                             {isLast && <Button className='btn-success' type="submit">Validar</Button>}
+    //                         </div>
+    //                     </Tab>
+    //                 );
+    //             } else {
+    //                 return
+    //             }
+    //         })}
+    //     </Tabs>
+    // );
 }
