dedad599bdd699eb8d105d42aeff5dfda805c053
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-03-20 01:59:13 -0300

    css prop type

--- src/components/json-viewer/JsonViewer.js
+++ src/components/json-viewer/JsonViewer.js
@@ -1,23 +1,22 @@
 import React, { Fragment } from 'react';
 import JSONPretty from 'react-json-pretty';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 const JsonViewer = ({ form, title, css }) => {
-
-    return (
-        <Fragment>
-            <div id="json-viewer" className="well" style={css}>
-                <legend>{title}</legend>
-                <JSONPretty id="json-pretty" data={form}></JSONPretty>
-            </div>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <div id='json-viewer' className='well' style={css}>
+        <legend>{title}</legend>
+        <JSONPretty id='json-pretty' data={form}></JSONPretty>
+      </div>
+    </Fragment>
+  );
 };
 
 JsonViewer.propTypes = {
-    form: PropTypes.any.isRequired,
-    title: PropTypes.string.isRequired,
-    css: PropTypes.string,
+  form: PropTypes.any.isRequired,
+  title: PropTypes.string.isRequired,
+  css: PropTypes.object
 };
 
 export default JsonViewer;
