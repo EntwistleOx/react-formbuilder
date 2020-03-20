6092eb002b57beb835df55531ff57f0b72d9e3d6
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-03-19 22:30:29 -0300

    typos

--- src/components/builder/toolkit/Toolkit.js
+++ src/components/builder/toolkit/Toolkit.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { createStep } from '../../../actions/form'
+import { createStep } from '../../../actions/form';
 import { Droppable } from 'react-beautiful-dnd';
 import ToolkitItems from './ToolkitItems';
 import PropTypes from 'prop-types';
@@ -9,40 +9,40 @@ import PropTypes from 'prop-types';
 // inline style to css file
 
 const Toolkit = ({ toolkitSchema, createStep }) => {
+  const onCreate = () => {
+    createStep();
+  };
 
-    const onCreate = () => {
-        createStep()
-    }
+  return (
+    <Droppable
+      key='ToolkitItems'
+      droppableId='ToolkitItems'
+      isDropDisabled={true}
+      type='builder'
+    >
+      {(provided, snapshot) => (
+        <div id='formbuilder-toolkit' className='well' ref={provided.innerRef}>
+          <a
+            onClick={onCreate}
+            href='#'
+            className='btn btn-default btn-sm btn-block btn-step'
+          >
+            <i class='far fa-file-alt'></i> Agregar Paso
+          </a>
 
-    return (
-        <Droppable key="ToolkitItems" droppableId="ToolkitItems" isDropDisabled={true} type="builder">
-            {(provided, snapshot) => (
-
-                <div id="formbuilder-toolkit"
-                    className="well"
-                    ref={provided.innerRef}
-                >
-                    <a
-                        onClick={onCreate}
-                        href="#"
-                        className="btn btn-default btn-sm btn-block btn-step"
-                    >
-                        <i className="fas fa-plus"></i> Agregar Paso
-                    </a>
-
-                    {toolkitSchema.map((item, index) => (
-                        <ToolkitItems key={item.id} item={item} index={index} />
-                    ))}
-                    {provided.placeholder}
-                </div>
-            )}
-        </Droppable>
-    )
-}
+          {toolkitSchema.map((item, index) => (
+            <ToolkitItems key={item.id} item={item} index={index} />
+          ))}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+  );
+};
 
 Toolkit.propTypes = {
-    toolkitSchema: PropTypes.array.isRequired,
-    createStep: PropTypes.func.isRequired,
-}
+  toolkitSchema: PropTypes.array.isRequired,
+  createStep: PropTypes.func.isRequired
+};
 
-export default connect(null, { createStep })(Toolkit)
+export default connect(null, { createStep })(Toolkit);
